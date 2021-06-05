e319d1017328dfe38fb36792423fe6b410c738f2
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-06-05 18:27:46 -0400

    Complete variables exercises (takeaways as comments)

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,10 +1,8 @@
 // variables3.rs
-// Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
-
-// I AM NOT DONE
 
 fn main() {
-    let x = 3;
+    // vars are immutable by default, instantiate them as mutable with "mut" keyword
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
