b79f916652cf6f0fc8e965012117cc2b4e478fdb
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-06-05 21:16:28 -0400

    Complete quiz1 (variables and functions)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -1,19 +1,18 @@
 // quiz1.rs
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-
 // Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
+// use if expression for function body (no ternary operator exists)
+fn calculate_apple_price(amount: i32) -> i32 {
+    if amount > 40 {
+        amount
+    } else {
+        amount * 2
+    }
+}
 
-// Put your function here!
-// fn ..... {
-
-// Don't modify this function!
-#[test]
+#[test] // apparently this is a test annotation
 fn verify_test() {
     let price1 = calculate_apple_price(35);
     let price2 = calculate_apple_price(40);
